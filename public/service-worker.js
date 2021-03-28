64cf4b75e0f302e6c058c8289ba9eb935503b032
Matthew Damron <mattdamron@msn.com>
2021-03-28 08:35:03 -0600

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'version_02';
+const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+// const DATA_CACHE_NAME = 'data-cache-v2';
 
 const FILES_TO_CACHE = [
     '/',
@@ -19,46 +20,44 @@ const FILES_TO_CACHE = [
     '/icons/icon-512x512.png'
 ];
 
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            return request || fetch(e.request)
+        })
+    )
+})
+
 // Cache resources
-self.addEventListener("install", function (e) {
+self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            console.log("installing cache : " + CACHE_NAME);
-            return cache.addAll(FILES_TO_CACHE);
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
         })
-    );
-});
+    )
+    self.skipWaiting();
+})
 
 // Delete outdated caches
-self.addEventListener("activate", function (e) {
+self.addEventListener('activate', function (e) {
     e.waitUntil(
         caches.keys().then(function (keyList) {
             // `keyList` contains all cache names under your username.github.io
             // filter out ones that has this app prefix to create keeplist
             let cacheKeeplist = keyList.filter(function (key) {
                 return key.indexOf(APP_PREFIX);
-            });
+            })
             // add current cache name to keeplist
             cacheKeeplist.push(CACHE_NAME);
 
-            return Promise.all(
-                keyList.map(function (key, i) {
-                    if (cacheKeeplist.indexOf(key) === -1) {
-                        console.log("deleting cache : " + keyList[i]);
-                        return caches.delete(keyList[i]);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// pull from cache first and fall back on network if cache does not exist
-// TODO: cache is registered and then created but will not load onto page when offline, seems like cache doesn't exist
-self.addEventListener("fetch", function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            return request || fetch(e.request);
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }));
         })
     );
 });
