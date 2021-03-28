1312b5b4ef990c2fe0d3ed5873ff0862ff33e37d
Matthew Damron <mattdamron@msn.com>
2021-03-28 10:42:59 -0600

    updated service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
-// const DATA_CACHE_NAME = 'data-cache-v2';
+const VERSION = 'Version_03';
+const CACHE_NAME = APP_PREFIX + VERSION + ' site';
+const DATA_CACHE_NAME = APP_PREFIX + VERSION + ' data';
 
 const FILES_TO_CACHE = [
     '/',
@@ -20,44 +20,70 @@ const FILES_TO_CACHE = [
     '/icons/icon-512x512.png'
 ];
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            return request || fetch(e.request)
-        })
-    )
-})
-
 // Cache resources
 self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
         })
     )
     self.skipWaiting();
 })
 
-// Delete outdated caches
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            // `keyList` contains all cache names under your username.github.io
-            // filter out ones that has this app prefix to create keeplist
-            let cacheKeeplist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            })
-            // add current cache name to keeplist
-            cacheKeeplist.push(CACHE_NAME);
+// Activate the service worker and remove old data from the cache
+self.addEventListener('activate', function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Removing old cache data', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
 
-            return Promise.all(keyList.map(function (key, i) {
-                if (cacheKeeplist.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i]);
-                    return caches.delete(keyList[i]);
+// Intercept fetch requests
+self.addEventListener('fetch', function (evt) {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+            caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            })
+            .catch(err => console.log(err))
+        );
+        return;
+    }
+
+    evt.respondWith(
+        fetch(evt.request).catch(function () {
+            return caches.match(evt.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (evt.request.headers.get('accept').includes('text/html')) {
+                    // return the cached home page for all requests for html pages
+                    return caches.match('/');
                 }
-            }));
+            });
         })
     );
 });
